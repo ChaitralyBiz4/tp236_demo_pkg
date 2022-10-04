4fdee422d9cf486ac04a350777742867a0fcb74b
Chaitrali Bhagyawant <chaitrali.bhagyawant@gaia.biz4solutions.com>
2022-10-04 16:52:18 +0530

    code change

--- src/stripeClassFiles/stripeAccountClass.php
+++ src/stripeClassFiles/stripeAccountClass.php
@@ -4,30 +4,49 @@ require_once('vendor/autoload.php');
 //global $stripe;
 class stripeAccountClass {
 
-    function initializeStripeClient($stripe_api_key){
-        $stripe = new \Stripe\StripeClient(["api_key" => $stripe_api_key]);
+    static $stripe_api_key;
+
+    public function __construct($stripe_api_key){
+        if($stripe_api_key == '')
+        {
+            return false;
+        }
+        $this -> stripe_api_key = $stripe_api_key;
+    }
+    
+    function initializeStripeClient(){
+        $stripe = new \Stripe\StripeClient(["api_key" => $this->stripe_api_key]);
        return $stripe;
     }
-    function setStripeApiKey($stripe_api_key){
-        \Stripe\Stripe::setApiKey($stripe_api_key);
+
+    function setStripeApiKey(){
+        \Stripe\Stripe::setApiKey($this->stripe_api_key);
 
     }
 
-    function createStripeConnectAccount($stripe_api_key,$account_type){
+    function createStripeConnectAccount($account_type){
+        if($account_type == '')
+        {
+            return false;
+        }
         //$stripe = new \Stripe\StripeClient(["api_key" => $stripe_api_key]);
         if($account_type != 'standard'){
             $error_message = "Use 'standard' as account type";
             return $error_message;
         }
-        $stripe = self::initializeStripeClient($stripe_api_key);
+        $stripe = self::initializeStripeClient($this->stripe_api_key);
         $stripe_account = $stripe->accounts->create([
                  'type' => $account_type,
          ]);
          return $stripe_account;
     }
 
-    function createStripeConnectAccountLink($stripe_api_key,$account_id){
-        $stripe = self::initializeStripeClient($stripe_api_key);
+    function createStripeConnectAccountLink($account_id){
+        if($account_id == '')
+        {
+            return false;
+        }
+        $stripe = self::initializeStripeClient($this->stripe_api_key);
         $account_link = \Stripe\AccountLink::create([
             'account' => $account_id,
             'refresh_url' => "", 
@@ -35,6 +54,16 @@ class stripeAccountClass {
             'type' => 'account_onboarding',
         ]);
         return $account_link;
+    }
+
+    function retrieveStripeAccount($account_id){
+        if($account_id == '')
+        {
+            return false;
+        }
+        $stripe = self::initializeStripeClient($this->stripe_api_key);
+        $stripe_account_data = $stripe->accounts->retrieve($account_id);
+        return $stripe_account_data;
     }
    
 
